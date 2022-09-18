fca639f680fe9b072868b494e554d4a682499546
gerlangiosoares <gerlangio@gmail.com>
2022-09-18 13:27:13 -0300

    Update db.class.php

--- db.class.php
+++ db.class.php
@@ -1,2 +1,24 @@
 <?php
+    //host
+    private $host = 'localhost';
+
+    //usuario
+    private $usuario = 'root';
+
+    //senha
+    private $senha = '';
+
+    //banco
+    private $database = 'twetter_clone';
+
+    public function conecta_mysql(){
+
+        //cria a conexao
+        $con = mysqli_connect(
+            $this->host,
+            $this->usuario,
+            $this->senha,
+            $this->database
+        );
+    }
 ?>
