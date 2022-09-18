df311d166bdc9ab8e68fd26d69a5ef90e85a767c
gerlangiosoares <gerlangio@gmail.com>
2022-09-18 18:17:52 -0300

    tw1

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+
+$con = mysqli_connect("localhost","root","twitter_clone");
+
+if(!$con){
+    die("Not Connected!");
+}
+
+?>
---
+++ header.php
@@ -0,0 +1,21 @@
+<?php
+ob_start();
+session_start();
+require_once('db.php');
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home / Twitter</title>
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+</head>
+<body>
+    
+</body>
+</html>
