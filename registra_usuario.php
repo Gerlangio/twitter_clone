e4db6bf0c71afccebdbe4bd0fe20148188dab7e8
gerlangiosoares <gerlangio@gmail.com>
2022-09-18 14:31:28 -0300

    Update registra_usuario.php

--- registra_usuario.php
+++ registra_usuario.php
@@ -1,7 +1,16 @@
 <?php
-    echo $_POST['usuario'];
-    echo '<br />';
-    echo $_POST['email'];
-    echo '<br />';
-    echo $_POST['senha'];
+  //requer uma vez
+    require_once('db.class.php');
+
+    $usuario = $_POST['usuario'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    $objDb = new db();
+    $link = $objDb->conecta_mysql();
+
+    $sql = "insert into usuarios(usuario, email, senha) values ('usuario', 'email', 'senha')";
+
+    //executar a query
+    mysqli_query($link, $sql);
 ?>
