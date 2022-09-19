d703694fc4325d9c8ec197e6b9c6f5b2396d92ed
gerlangiosoares <gerlangio@gmail.com>
2022-09-18 21:43:57 -0300

    Update tweet.php

--- tweet.php
+++ tweet.php
@@ -5,8 +5,7 @@ $data = mysqli_query($con,$query);
 
 while($row = mysqli_detch_assoc($data)){
     $post_text = $row['post_text'];
-    $post_date = $row['post_text'];
-}
+    $post_date = $row['post_date'];
 
 ?>
 
@@ -23,6 +22,21 @@ while($row = mysqli_detch_assoc($data)){
 
         <p class="tweet__text"><?php echo $post_text; ?></p>
 
-        
+        <div class="tweet__icons">
+            <a href=""><i class="far fa-comment"></i></a>
+            <a href=""><i class="fa fa-replay"></i></a>
+            <a href=""><i class="far fa-heart"></i></a>
+            <a href=""><i class="fa fa-upload"></i></a>
+            <a href=""><i class="far fa-chart-bar"></i></a>
+        </div>
+    </div>
+
+    <div class="tweet__del">
+        <div class="dropdown">
+            <button class="dropbtn"><span class="fa fa-ellipsis-h"></span></button>
+            <div class="dropdow-content">
+                <a href="index.php?del=<?php echo $row['post_id']; ?>"><i class="far fa-trash-alt"></i><span>Delete</span></a>
+            </div>
+        </div>
     </div>
 </div>
