829e173058646808f48ea6896a4f5e9f20090c1f
gerlangiosoares <gerlangio@gmail.com>
2022-09-19 20:38:53 -0300

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,12 @@
 <?php
+    define('HOST', 'localhost');
+    define('USER', 'root');
+    define('PASS', '');
+    define('BASE', 'twitter_clone');
 
-$con = mysqli_connect("localhost","root","","twitter_clone");
+
+$con = new MySQLi(HOST,USER,PASS,BASE);
 
 if(!$con){
     die("Not Connected!");
 }
-
-?>
